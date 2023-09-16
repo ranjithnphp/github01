d852b5fbf735fee935f9eacc0fbadb7b21918f1f
RANJITHMANI N <144892468+ranjithnphp@users.noreply.github.com>
2023-09-16 10:41:13 +0530

    Updated basics.php

--- basics.php
+++ basics.php
@@ -4,18 +4,12 @@
 
 $name = "GaNeSh";
 
-echo str_repeat($name,5);
-echo "<br>";
-echo strpos($name,"N");
-echo "<br>";
-echo strtoupper($name);
-echo "<br>";
-echo strtolower($name);
-echo "<br>";
-echo str_word_count($name);
-echo "<br>";
-echo strlen($name);
-echo "<br>";
+echo str_repeat($name,5);echo "<br>";
+echo strpos($name,"N");echo "<br>";
+echo strtoupper($name);echo "<br>";
+echo strtolower($name);echo "<br>";
+echo str_word_count($name);echo "<br>";
+echo strlen($name);echo "<br>";
 echo strstr($name,"e");
 ?>
 
